9f16fa14d9a2417ccfaa0e35384c3c78e262d604
RadiooGaga <mariahkatt@gmail.com>
2024-07-09 10:14:30 +0200

    second commit

--- src/components/Character/Character.jsx
+++ src/components/Character/Character.jsx
@@ -4,31 +4,40 @@ import { useEffect, useState } from "react";
 const Peticion = () => {
 
     const [characters, setCharacters] = useState([]);
-    const [rotatedCharacters, setRotatedCharacters] = useState(false);
+    const [rotatedCharacterId, setRotatedCharacterId] = useState(false);
 
     useEffect(() => {
-        fetch("https://rickandmortyapi.com/api/character")
+        fetch(`https://rickandmortyapi.com/api/character`)
         .then((res) => res.json())
         .then((data) => setCharacters(data.results));
     }, []);
    
     const clickOnImage = (id) => {
-      setRotatedCharacters(id);
+      setRotatedCharacterId(rotatedCharacterId === id ? false : id); 
+      /* si es igual a id, la imagen ya está rotada. Si ya está rotada, el siguiente click 
+      volverá la imagen al estado inicial*/
     };
 
     return ( 
-    
-    <div className='charactersContainer'>
-        {console.log(characters)}
-        {characters.map((character) => (
-            <img className={`imgCharacter ${rotatedCharacters === character.id ? 'rotated' : ''}`}
-            key={character.id} 
-            src={character.image} 
-            onClick={() => clickOnImage(character.id)} 
-            />
+        <div className='charactersContainer'>
+            {characters.map((character) => (
+                <div className='characterCard' key={character.id}>
+                    <img 
+                        className={`imgCharacter ${rotatedCharacterId === character.id ? 'rotated' : ''}`}
+                        src={character.image} 
+                        alt={character.name}
+                        onClick={() => clickOnImage(character.id)} 
+                    />
+                    <div className='characterDetails'>
+                        <h2>{character.name}</h2>
+                        <p>Status: {character.status}</p>
+                        <p>Species: {character.species}</p>
+                        <p>Gender: {character.gender}</p>
+                        <p>Origin: {character.origin.name}</p>
+                    </div>
+                </div>
             ))}
-
-    </div>
+        </div>
     );
 }
 
